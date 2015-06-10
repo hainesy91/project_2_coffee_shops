b8137fb11e40b1290ab42179d2f55743b3d2194e
Christopher Haines <hainesy91@gmail.com>
2015-06-10 10:01:23 +0100

    added a show page for all of the coffee types created

--- app/controllers/coffee_types_controller.rb
+++ app/controllers/coffee_types_controller.rb
@@ -12,10 +12,14 @@ class CoffeeTypesController < ApplicationController
     coffeetype = CoffeeType.new params[:coffee_type].permit(:name, :origin, :price)
 
     if coffeetype.save
-      redirect_to coffee_types_path
+      redirect_to coffee_type_path(coffeetype)
     else
       render 'new'
     end
+  end
+
+  def show
+    @coffeetype = CoffeeType.find(params[:id])
   end
 
 end
