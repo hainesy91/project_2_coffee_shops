1acc82d5c04139fb3dedaac02cacf9df26c62a5b
Christopher Haines <hainesy91@gmail.com>
2015-09-08 18:23:53 +0100

    added nav bar for the home link

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,13 @@
 c1 = CoffeeType.create(name: 'Espresso', origin: 'Guatamala', price: '2.0')
 c2 = CoffeeType.create(name: 'Flat White', origin: 'Mexico', price: '3.0')
 
-c3 = CoffeeShop.create(name: 'Dose Espresso', address: '18 Long Acre', phone_number: '0204484302' )
+c3 = CoffeeShop.create(name: 'Cafe Nero', address: '18 Long Acre', phone_number: '0204484302' )
+c4 = CoffeeShop.create(name: 'Costa', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
+c5 = CoffeeShop.create(name: 'look mum no hands', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
+c6 = CoffeeShop.create(name: 'Tapped and Packed', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
+c7 = CoffeeShop.create(name: 'Soho Grind', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
+c8 = CoffeeShop.create(name: 'Starbucks', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
+c9 = CoffeeShop.create(name: 'Grind Espresso', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
+
 
 puts 'Seeded!'
