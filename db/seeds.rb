d6209f0df2c8caf79e4fce503a0d7c5ef06615a6
Christopher Haines <hainesy91@gmail.com>
2015-10-15 16:32:37 +0100

    updated seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,9 +14,9 @@ c3 = CoffeeShop.create(name: 'Cafe Nero', address: '18 Long Acre', phone_number:
 c4 = CoffeeShop.create(name: 'Costa', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
 c5 = CoffeeShop.create(name: 'look mum no hands', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
 c6 = CoffeeShop.create(name: 'Tapped and Packed', address: '18 Long Acre', phone_number: '0204484302', image: 'https://s-media-cache-ak0.pinimg.com/736x/6f/63/03/6f6303d466bb2abc3d5ad013b2cd5fb6.jpg' )
-c7 = CoffeeShop.create(name: 'Soho Grind', address: '18 Long Acre', phone_number: '0204484302', image: 'http://www.hospitalityandcateringnews.com/wp-content/uploads/Soho-Grind.jpg' )
-c8 = CoffeeShop.create(name: 'Starbucks', address: '18 Long Acre', phone_number: '0204484302', image: 'http://www.weekendnotes.co.uk/im/004/08/starbucks-coffee1.JPG' )
-c9 = CoffeeShop.create(name: 'AllPress Espresso', address: '18 Long Acre', phone_number: '0204484302', image: 'http://thecoffeevine.com/wp-content/uploads/2013/08/img_5393.jpg' )
+c7 = CoffeeShop.create(name: 'Soho Grind', address: '19 Beak St', phone_number: '020 7287 7073', image: 'http://www.hospitalityandcateringnews.com/wp-content/uploads/Soho-Grind.jpg' )
+c8 = CoffeeShop.create(name: 'Starbucks', address: '10 Wimbledon Hill Rd', phone_number: '020 8944 6864', image: 'http://www.weekendnotes.co.uk/im/004/08/starbucks-coffee1.JPG' )
+c9 = CoffeeShop.create(name: 'AllPress Espresso', address: '9 Club Row', phone_number: '02077491780', image: 'http://thecoffeevine.com/wp-content/uploads/2013/08/img_5393.jpg' )
 
 
 puts 'Seeded!'
